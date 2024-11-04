5795cbf132e444cdd0d45d79eb2dd2c17be68a7e
FarukErat <farukerat1327@gmail.com>
2024-11-04 14:29:20 +0300

    rename enter_sms_code to enter_code

--- ahk/connect_to_ivanti.bat
+++ ahk/connect_to_ivanti.bat
@@ -3,4 +3,4 @@
 C:\Users\%USERNAME%\Documents\AutoHotkey\send_sms.exe
 echo "Enter SMS code: "
 set /p sms_code=
-C:\Users\%USERNAME%\Documents\AutoHotkey\enter_sms_code.exe %sms_code%
+C:\Users\%USERNAME%\Documents\AutoHotkey\enter_code.exe %sms_code%
